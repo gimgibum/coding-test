b3fd5e8482886e2b816ede9ab819e4652455e2ff
gimgibum <koreadodomp@gmail.com>
2026-06-13 00:07:12 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.07 ms, Memory: 43.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ JavaScript/프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,7 +1,3 @@
 function solution(my_string) {
-    my_string = my_string.toLowerCase().split('');
-    my_string = my_string.sort((a,b) => a.localeCompare(b));
-    var answer = '';
-    for(x of my_string) answer += x;
-    return answer;
+    return my_string.toLowerCase().split('').sort().join('');
 }
