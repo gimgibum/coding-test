1c40e89d44067740866b372b5be8c95ffbb3be2e
gimgibum <koreadodomp@gmail.com>
2026-06-24 09:41:11 +0900

    [level 1] Title: 하샤드 수, Time: 0.08 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,8 @@
+function solution(x) {
+    let hashard = x.toString().split('');
+    let sum = 0;
+    for(s of hashard){
+        sum += Number(s);
+    }
+    return x%sum===0 ? true : false;
+}
